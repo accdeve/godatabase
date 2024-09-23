8700b9c6d52668e7e311501c2aa2d3eec4f5febe
Aditya Ibnu Arif <akunkuliahadit@gmail.com>
2024-09-23 11:03:39 +0700

    add function getDb

--- database/db.go
+++ database/db.go
@@ -2,16 +2,31 @@ package database
 
 import (
 	"database/sql"
+	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
 
-func Init(){
+func Init() {
 	var err error
-	DB, err = sql.Open("mysql","root:@/godatabase")
 
-	if err != nil{
-		panic(err.Error())
+	// Inisialisasi koneksi ke database
+	DB, err = sql.Open("mysql", "root:@/godatabase")
+	if err != nil {
+		log.Fatalf("Error opening database connection: %v", err)
 	}
+
+	// Tes koneksi
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	log.Println("Database connection established")
+}
+
+func GetDB() *sql.DB {
+	return DB
 }
